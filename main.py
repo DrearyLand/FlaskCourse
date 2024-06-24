a0ec699451b6506f5ec377edc3b9129533f5cc13
DrearyLand <drearyland22021@gmail.com>
2024-06-24 10:28:23 -0600

    App Factory, and file for testing

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
-from flask import Flask, request, make_response, redirect, render_template, session, url_for
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from flask import request, make_response, redirect, render_template, session, url_for, flash
+import unittest
 
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
+from app import create_app
+from app.forms import LoginForm
 
-app.config['SECRET_KEY'] = 'SUPER SECRETO'
+app = create_app()
 
 todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor']
 
-class LoginForm(FlaskForm):
-    username = StringField('Nombre de usuario', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Enviar')
-    
+
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
 
 @app.errorhandler(404)
 def not_found(error):
@@ -51,6 +48,7 @@ def hello():
         username = login_form.username.data
         session['username'] = username
         
+        flash('Nombre de usuario registrado con exito')
         return redirect(url_for('index'))
     
     return render_template('hello.html', **context)
